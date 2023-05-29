489b5d3f054b3fc3a60d0f2caa144fd90c7cc659
Esteban Ruiz <rubianruiz05@my.com>
2023-05-29 00:07:53 -0500

    1: Client project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpLink } from 'apollo-angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+import { LoginModule } from './auth/login/login.module';
+import { SignUpModule } from './auth/sign-up/sign-up.module';
+import { InMemoryCache } from '@apollo/client/core';
+import { HttpClientModule } from '@angular/common/http';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    HeaderModule,
+    LoginModule,
+    SignUpModule,
+    ApolloModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'api/graphql',
+          }),
+        };
+      },
+      deps: [HttpLink]
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
